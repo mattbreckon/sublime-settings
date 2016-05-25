fc5e623d7a40026df1c928bf9a23f782ff6d4fdc
Matt Breckon <matt@mattbreckon.com>
2016-05-25 00:55:29 -0400

    Additional preferences, keybindings and packages

--- keymap.js
+++ keymap.js
@@ -1,3 +1,14 @@
 [
-  { "keys": ["super+shift+r"], "command": "reveal_in_side_bar" },
+  {
+    "keys": ["super+shift+r"],
+    "command": "reveal_in_side_bar"
+  },
+  {
+    "keys": ["ctrl+super+up"],
+    "command": "swap_line_up"
+  },
+  {
+    "keys": ["ctrl+super+down"],
+    "command": "swap_line_down"
+  }
 ]
